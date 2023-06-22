92b077fb2e5dafcd03f3be2faece0566ab1f58c3
soundjester <soundjester@users.noreply.github.com>
2023-06-22 18:21:56 -0400

    added code comment

--- old.reddit.user.js
+++ old.reddit.user.js
@@ -17,7 +17,7 @@
 	} catch (_er) {
 		isLemmy = false;
 	}
-	// Lemmy to old.Reddit style reformats (to be used for custom stylesheet at a later date)
+	// Lemmy to old.Reddit style reformats (portable custom stylesheet)
 	if (isLemmy) {
 		const css = `
 			/**************************/
