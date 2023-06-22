04c5ba0d17b54d0440dd4e174e0d9518b3390ed4
soundjester <soundjester@users.noreply.github.com>
2023-06-22 18:25:02 -0400

    Update old.reddit.user.js

--- old.reddit.user.js
+++ old.reddit.user.js
@@ -3,7 +3,6 @@
 // @namespace    https://github.com/soundjester/lemmy_monkey/
 // @description  Reformat Lemmy instances to the style of old.reddit - larger thumbnails
 // @version      1.9.5.3
-// @description  Reformat widescreen desktop to look more like Reddit
 // @author       mershed_perderders, DarkwingDuck, dx1@lemmy.world, Djones4822
 // @updateURL    https://github.com/soundjester/lemmy_monkey/raw/main/old.reddit.user.js
 // @downloadURL  https://github.com/soundjester/lemmy_monkey/raw/main/old.reddit.user.js
