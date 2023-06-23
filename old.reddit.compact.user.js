0f2f4d75c9bcffdf4d38761f3525517cb3cb1a78
soundjester <soundjester@users.noreply.github.com>
2023-06-22 20:32:30 -0400

    version number update

--- old.reddit.compact.user.js
+++ old.reddit.compact.user.js
@@ -2,7 +2,7 @@
 // @name         Compact Lemmy to Old.Reddit Re-format (Observer)
 // @namespace    https://github.com/soundjester/lemmy_monkey/
 // @description  Reformat Lemmy instances to the style of old.reddit - compact view
-// @version      1.9.5.3
+// @version      1.10
 // @author       mershed_perderders, DarkwingDuck, dx1@lemmy.world, Djones4822
 // @updateURL    https://github.com/soundjester/lemmy_monkey/raw/main/old.reddit.compact.user.js
 // @downloadURL  https://github.com/soundjester/lemmy_monkey/raw/main/old.reddit.compact.user.js
