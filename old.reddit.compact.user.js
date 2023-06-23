ce6a55a00a21a0a8b0fb3b5aaf1e69984e05d505
Mershed Perderders <soundjester@users.noreply.github.com>
2023-06-23 15:52:27 -0400

    major overhaul of CSS for v0.18

--- old.reddit.compact.user.js
+++ old.reddit.compact.user.js
@@ -10,52 +10,356 @@
 // source-url    https://github.com/soundjester/lemmy_monkey/
 // ==/UserScript==
 (function() {
-    'use strict';
-    //Thank you God!
-    var isLemmy;
-    try {
-        isLemmy = document.head.querySelector("[name~=Description][content]").content === "Lemmy";
-    } catch (_er) {
-        isLemmy = false;
-    }
-
-    //special thanks to StackOverflow - the one true source of all code, amen.
-    function GM_addStyle(css) {
-        const style = document.getElementById("GM_addStyleBy8626") || (function() {
-            const style = document.createElement('style');
-            style.type = 'text/css';
-            style.id = "GM_addStyleBy8626";
-            document.head.appendChild(style);
-            return style;
-        })();
-        const sheet = style.sheet;
-        sheet.insertRule(css, (sheet.rules || sheet.cssRules || []).length);
-    }
-
-  // Lemmy to old.Reddit style reformats (to be used for custom stylesheet at a later date)
+	'use strict';
+	//Thank you God!
+	var isLemmy;
+	try {
+		isLemmy = document.head.querySelector("[name~=Description][content]").content === "Lemmy";
+	} catch (_er) {
+		isLemmy = false;
+	}
+	// Lemmy to old.Reddit style reformats (portable custom stylesheet)
 	if (isLemmy) {
-		//main container
-		GM_addStyle(".container, .container-lg, .container-md, .container-sm, .container-xl { max-width: 100% !important; }");
-		// bootstrap main column widths
-		GM_addStyle(".col-md-4 { flex: 0 0 20% !important; max-width: 20%; }"); //sidebar width
-		GM_addStyle(".col-md-8 { flex: 0 0 80% !important; max-width: 80%; }"); //main post area (for widescreen)
+		const css = `
+			/**************************/
+			/* NSFW automatic un-blur */
+			/**************************/
+			 .img-blur {
+				 filter: none !important;
+				 -webkit-filter: none !important;
+				 -moz-filter: none !important;
+				 -o-filter: none !important;
+				 -ms-filter: none !important;
+			}
+			/***************************/
+			/* bootstrap column widths */
+			/***************************/
+			/*main container*/
+			 .container, .container-lg, .container-md, .container-sm, .container-xl {
+				 max-width: 100% !important;
+			}
+			/*sidebar width*/
+			 .col-md-4 {
+				 flex: 0 0 20% !important;
+				 max-width: 20%;
+				 padding-right: unset !important;
+			}
+			/*main post area (witdh optimized for widescreen)*/
+			 .col-md-8 {
+				 flex: 0 0 80% !important;
+				 max-width: 80%;
+			}
+			 .col-sm-2 {
+				 flex: 0 0 10% !important;
+				 max-width: 10%;
+			}
+			 .col-sm-9 {
+				 flex: 0 0 80% !important;
+				 max-width: 80%;
+			}
+			 .col-8 {
+				 max-width: 100% !important;
+			}
+			/* specific column combos that need padding adjustment*/
+      /*
+			 .col-12.col-md-8 {
+				 padding-left: unset !important;
+			}
+      */
+      /*
+			 .col-12.col-sm-9 {
+				 padding-left: unset !important;
+			}
+      */
+			/* navbar padding*/
+			 .navbar {
+				 /*padding-left: 0 !important;*/
+				 padding-right: 1em !important;
+			}
+			 .navbar-nav {
+				 margin-top: 0px !important;
+				 margin-bottom: 0px !important;
+			}
+			/* control vertical padding*/
+			 .mb-1, .my-1 {
+				 margin-bottom: 0.1rem !important;
+			}
+			 .mb-2, .my-2 {
+				 margin-bottom: 0.1rem !important;
+			}
+			 .mt-3, .my-3 {
+				 margin-top: 0.1rem !important;
+			}
+			 .mt-4, .my-4 {
+				 margin-top: 0.1rem !important;
+			}
+			/***************/
+			/* voting area */
+			/***************/
+			/*can be modified as you like*/
+			 .vote-bar {
+				 font-size: 0.85em !important;
+				 flex: 0 0 4% !important;
+				 max-width: 4% !important;
+				 margin-top:unset !important;
+			}
+			/******************/
+			/* thumbnail area */
+			/******************/
+			/*keep thumbnails as square as we can and about the size of each post row*/
+			 .post-media {
+				 min-width: 70px !important;
+				 max-width: 70px !important;
+				 margin-right: 1em !important;
+			}
+			 .thumbnail {
+				 min-height: 70px !important;
+				 max-height: 70px !important;
+				 min-width: 70px !important;
+				 max-width: 70px !important;
+			}
+			/*this is needed for videos/gifs*/
+			 .embed-responsive {
+				 min-height: 70px !important;
+				 max-height: 70px !important;
+				 min-width: 70px !important;
+				 max-width: 70px !important;
+			}
+			/*******************/
+			/* main page posts */
+			/*******************/
+			/* post title font size*/
+			 .h5, h5 {
+				 font-size: 1rem !important;
+				 margin-bottom: 0.1rem !important;
+			}
+      .small, small {
+        font-size: 80%;
+        font-weight: 400;
+      }
+      .text-muted {
+        font-size: .8rem;
+      }
+			/*can be adjusted smaller, but beyond .25 is gets too tight and individual post spacing starts to appear overlapping*/
+			 .post-listing {
+				 margin: 0.25rem 0 !important;
+				 padding: 0.25rem 0 !important;
+			}
+			 .post-listing picture img.rounded-circle {
+				 width: 1.25rem;
+				 height: 1.25rem;
+			}
+			/*thumbnail width control (keep it square, dang it!)*/
+			 .post-listing .d-none .row .col-sm-2 {
+				 max-width: 100px;
+			}
+			 .post-listing .d-none .row .col-sm-9 {
+				 display: flex;
+				 align-items: unset !important;
+			}
+			/*comment number and fediverse/lemmy links*/
+			 .ps-0 {
+				 font-size: 0.75rem !important;
+			}
+			/*the below .btn is deprecated as .py-0 (above) provides more consistent spacing;
+			 however, some may prefer the look of smaller text on buttons*/
+			/*.btn {
+				 font-size:0.75rem !important;
+			}
+			/*media collapse/expand button - appears after post title for offsite links that have a thumbnail*/
+			 .btn.btn-sm.text-monospace.text-muted.d-inline-block {
+				 padding-top: 0;
+				 padding-bottom: 0;
+			}
+      .text-body.mt-2.d-block{
+        font-size: 0.8rem;
+        display: none !important;
+      }
+			/************/
+			/* comments */
+			/************/
+      /* restrict post and comment width - adjust to preference */
+      .md-div {
+        max-width: 940px;
+      }
+			/*top comment doesn't need to hug the comment sort buttons.*/
+			 .comments:first-child {
+				 margin-top: 0.5rem !important;
+			}
+			/*allow top-level comment box to be resized*/
+			 div > textarea {
+				 resize: both !important;
+			}
+      .mb-3.row {
+        max-width: 940px;
+      }
+			/***********/
+			/* sidebar */
+			/***********/
+			 #sidebarContainer {
+  				 padding-right: 1em;
+			}
+			/******************************/
+			/* entire page display tweaks */
+			/******************************/
+			 #app > div > .container-lg {
+				 margin-left: 1em !important;
+				 max-width: 99% !important;
+				 margin-left: unset !important;
+			}
+			 #app > nav > .container-lg {
+				 max-width: 100% !important;
+			}
+			 #app > navbar > .container-lg {
+				 margin-left: unset !important;
+			}
+			/* post index layout*/
+			 #app > .mt-4 > .container-lg hr.my-3 {
+				 display: none;
+			}
+			 #app > .mt-4 > .container-lg > .row {
+				 margin: unset !important;
+			}
+			/* post layout*/
+			 #app > .mt-4 > .container-lg > .row > main {
+				 max-width: 100%;
+			}
+			 #app > .mt-4 > .container-lg > .row > .col-md-8 {
+				 width: calc(100% - 450px);
+			}
+			 #app > .mt-4 > .container-lg > .row > .col-md-4 {
+				 width: 450px;
+			}
+      hr {
+        display: none;
+      }
+			/* Fix user drop down menu position*/
+			 .dropdown-content {
+				 right: 0px;
+			}
+			/* Profile and Community Banner size */
+			.position-relative.mb-2 {
+				max-width: 730px;
+			}
+			/*table styles - primarily used on the "Communities" page*/
+			 .table-responsive {
+				 margin-top: 0.5em;
+			}
+			 .table-sm td, .table-sm th {
+				 padding: 0.1rem;
+				 vertical-align: middle;
+			}
+			/* some instances include a tag line*/
+			 #tagline {
+				 margin-left: 1em;
+			}
+			/**********************************************/
+			/** Specific screen size (mobile) adjustments */
+			/**********************************************/
+			 @media screen and (min-width: 1981px) {
+				 #app {
+					 max-width: 1980px;
+					 margin-left: auto;
+					 margin-right: auto;
+				}
+			}
+			 @media screen and (min-width: 1200px) and (max-width: 1640px) {
+				 .col-md-4 {
+					 flex: 0 0 25% !important;
+					 max-width: 25%;
+				}
+				 .col-md-8 {
+					 flex: 0 0 75% !important;
+					 max-width: 75%;
+				}
+			}
+			 @media screen and (max-width: 1199px) and (min-width: 992px) {
+				 .col-md-4 {
+					 flex: 0 0 33.3333% !important;
+					 max-width: 33.3333%;
+				}
+				 .col-md-8 {
+					 flex: 0 0 66.6666% !important;
+					 max-width: 66.6666%;
+				}
+				 .navbar {
+					 padding-right: 0em !important;
+				}
+			}
+			 @media screen and (max-width: 1199px) and (min-width: 576px) {
+				 .col-1 {
+					 flex: 0 0 6% !important;
+					 max-width: 6% !important;
+				}
+				 .col-12.col-sm-9 {
+					 padding-left: 1em !important;
+				}
+			}
+			 @media screen and (max-width: 940px) and (min-width: 576px) {
+				 .col-md-8 {
+					 flex: 0 0 100% !important;
+					 max-width: 100%;
+				}
+			}
+			 @media screen and (max-width: 768px) and (min-width: 576px) {
+				 .pl-1, .px-1 {
+					 padding-left: unset !important;
+				}
+				 .pl-3, .px-3 {
+					 padding-left: 1rem !important;
+					 padding-right: 1rem !important;
+				}
+			}
+			 @media screen and (max-width: 575px) {
+				 .col-12.col-md-8 {
+					 padding-right: 0em !important;
+				}
+				 .col-8 {
+					 flex: 0 0 75% !important;
+					 max-width: 75%;
+				}
+				 .col-4 {
+					 flex: 0 0 25% !important;
+					 max-width: 25%;
+					 justify-content: flex-end !important;
+					 display: flex !important;
+				}
+			 }`
+
+		const styleTag = document.createElement('style');
+		styleTag.appendChild(document.createTextNode(css));
+		document.head.appendChild(styleTag);
+
+		/*move tagline element to be between navbar and content nav buttons*/
+		var div_list = document.querySelectorAll("div#app");
+		var div_array = [...div_list];
+
+		div_array.forEach(container => {
+			var firstTargDiv = container.querySelector("div#tagline");
+			var secondTargDiv = container.querySelector(".mt-4.p-0.fl-1");
+			//-- Swap last to first.
+			if(firstTargDiv !== null && secondTargDiv !== null){
+				container.insertBefore(firstTargDiv, secondTargDiv);
+			}
+		});
+		/*Fix navbar craziness involving the search button*/
+		var nav_list = document.querySelectorAll(".navbar-nav");
+
+		[...nav_list].forEach(container => {
+			if (!container.className.includes("my-2") && !container.className.includes("ml-auto") && !container.className.includes("ml-1")) {
+				container.className += " " + "my-2";
+			}
+		});
+
+		var link_list = document.querySelectorAll("div.row > div.col-12");
 		
-		// voting area
-		GM_addStyle(".vote-bar { flex: 0 0 4% !important; max-width: 4% !important;}");
-		
-		//control size of thumbnails
-		GM_addStyle(".post-media { min-width: 70px !important; max-width: 70px !important; margin-right:1em !important }"); //keep thumbnails as square as we can and about the size of each post row
-		GM_addStyle(".thumbnail { min-height: 70px; max-height: 70px; min-width: 70px; max-width: 70px; }"); //keep thumbnails as square as we can and about the size of each post row
-		//GM_addStyle(".embed-responsive-item { min-height: 70px; max-height: 70px; min-width: 70px; max-width: 70px; }"); //this may be needed for videos/gifs - nothing to test on yet
-		GM_addStyle(".img-cover.img-icon.me-2 {   max-height: 20px;   max-width: 20px; }"); //community link icons
-		
-		/*************
-		* font sizes *
-		**************/
-		// size of vote counter
-		GM_addStyle(".vote-bar { font-size: .85em; }");
-		GM_addStyle(".h5, h5 {  font-size: 1rem !important; }"); //post title
-		GM_addStyle(".list-inline.mb-1.text-muted.small.mt-2 { font-size: .75em; }"); //author and community
-		GM_addStyle(".d-flex align-items-center justify-content-start flex-wrap text-muted {font-size: .75em; }"); //number of comments
+		[...link_list].forEach(container => {
+			var linkItemA = container.querySelectorAll("p.d-flex.text-muted.align-items-center.gap-1.small.m-0");
+			//var linkItemB = container.querySelectorAll(".small");
+			[...linkItemA].forEach(container => {
+				container.className += " " + "d-inline-flex";
+				//container.parentNode.insertBefore(container," • ")
+			});
+		});
 	}
 })();
