6c8f9dfc7d9cd37398e4e56fb8c3255fec56b7ee
Mershed Perderders <soundjester@users.noreply.github.com>
2023-06-26 13:00:48 -0400

    align width of dropdown to parent element

--- old.reddit.compact.user.js
+++ old.reddit.compact.user.js
@@ -2,7 +2,7 @@
 // @name         Compact Lemmy to old.Reddit Re-format (Lemmy v0.18)
 // @namespace    https://github.com/soundjester/lemmy_monkey
 // @description  Reformat widescreen desktop to look more like Reddit
-// @version      2.3.1
+// @version      2.4
 // @author       mershed_perderders, DarkwingDuck, dx1@lemmy.world, Djones4822, Jakylla
 // @updateURL    https://github.com/soundjester/lemmy_monkey/raw/main/old.reddit.compact.user.js
 // @downloadURL  https://github.com/soundjester/lemmy_monkey/raw/main/old.reddit.compact.user.js
@@ -327,6 +327,9 @@
 			 .dropdown-content {
 				 right: 0px;
 			}
+			.dropdown-menu.show {
+				width: 100%;
+			}   
 			/* Profile and Community Banner size */
 			 .position-relative.mb-2 {
 				 max-width: 730px;
